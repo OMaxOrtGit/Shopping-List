c04c7575a395715a689d35b3654f17f47fb40be4
max ortman <mortman2299@stu.d214.org>
2019-02-11 15:08:12 -0600

    added connections and table views

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var newItemTextView: UINavigationItem!
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func newItemButtonPress(_ sender: Any) {
+        
+    }
+    
 }
 
